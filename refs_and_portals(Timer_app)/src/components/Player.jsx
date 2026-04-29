d9f2ebb3a7d09c8c33f9599f886a82c246c02447
MisbahAziz295 <misbah.aziz@expertflow.com>
2026-04-29 14:18:06 +0500

    use of useRef

--- refs_and_portals(Timer_app)/src/components/Player.jsx
+++ refs_and_portals(Timer_app)/src/components/Player.jsx
@@ -1,23 +1,32 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 export default function Player() {
+  const playerName = useRef();
+
   const [enteredPlayerName, setEnteredPlayerName] = useState(null);
-  const [submittedPlayerName, setSubmittedPlayerName] = useState(false);
-  function handleChange(e) {
-    setSubmittedPlayerName(false);
-    setEnteredPlayerName(e.target.value);
-  }
+  // const [submittedPlayerName, setSubmittedPlayerName] = useState(false);
+  // function handleChange(e) {
+  //   setSubmittedPlayerName(false);
+  //   setEnteredPlayerName(e.target.value);
+  // }
 
   function handleClick() {
-    setSubmittedPlayerName(true);
+    // setSubmittedPlayerName(true);
+    setEnteredPlayerName(playerName.current.value);
   }
 
   return (
     <section id="player">
       <h2>
-        Welcome {submittedPlayerName ? enteredPlayerName : "unknown entity"}
+        {/* Welcome {submittedPlayerName ? enteredPlayerName : "unknown entity"} */}
+        Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"}
       </h2>
       <p>
-        <input type="text" value={enteredPlayerName} onChange={handleChange} />
+        <input
+          ref={playerName}
+          type="text"
+          // value={enteredPlayerName}
+          // onChange={handleChange}
+        />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
