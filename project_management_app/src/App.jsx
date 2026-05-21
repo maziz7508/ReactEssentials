2fc997f40a8ecd34fd44a5f6d8725ac330fbf180
MisbahAziz295 <misbah.aziz@expertflow.com>
2026-05-22 00:15:50 +0500

    NewProject and Input Components created

--- project_management_app/src/App.jsx
+++ project_management_app/src/App.jsx
@@ -1,9 +1,12 @@
 import ProjectSidebar from "./components/ProjectSidebar";
+import NewProject from "./components/NewProject";
+
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
       <ProjectSidebar />
+      <NewProject />
     </main>
   );
 }
