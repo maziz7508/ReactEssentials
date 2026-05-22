e9357c3d0dbe9a2a4c181b4f8a3aa2daa7e5d242
MisbahAziz295 <misbah.aziz@expertflow.com>
2026-05-22 21:16:37 +0500

    Render the noProject and newProject components conditionally

--- project_management_app/src/App.jsx
+++ project_management_app/src/App.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import ProjectSidebar from "./components/ProjectSidebar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prev) => ({ ...prev, selectedProjectId: null }));
+  }
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
       {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
-      <ProjectSidebar />
-      <NewProject />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
+      {/* <NoProjectSelected onStartAddProject={handleStartAddProject} /> */}
+      {/* <NewProject /> */}
     </main>
   );
 }
