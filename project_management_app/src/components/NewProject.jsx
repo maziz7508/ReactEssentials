7eb8cc2865d49565954753058f0056f40fd186fa
MisbahAziz295 <misbah.aziz@expertflow.com>
2026-06-02 19:53:21 +0500

    Validating User Input & Showing an Error Modal via useImperativeHandle

--- project_management_app/src/components/NewProject.jsx
+++ project_management_app/src/components/NewProject.jsx
@@ -1,6 +1,8 @@
 import Input from "./Input";
 import { useRef } from "react";
+import Modal from "./Modal";
 export default function NewProject({ onAdd }) {
+  const modal = useRef();
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -8,6 +10,17 @@ export default function NewProject({ onAdd }) {
     const enteredTitle = title.current.value;
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
+
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      //   alert("Please enter a title");
+      //   return;
+      modal.current.open();
+      return;
+    }
 
     //Validation
     onAdd({
@@ -17,25 +30,33 @@ export default function NewProject({ onAdd }) {
     });
   }
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            {" "}
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={handleSave}
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        {/* <p>
+    <>
+      <Modal ref={modal} buttonCaption="Okay"> 
+        <h2>
+         Invalid Input
+      </h2>
+      <p>Oops .... looks like you forgot to enter a value.</p>
+      <p>Please make sure sure you provide a valid value for every input field.</p>
+        </Modal>>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              {" "}
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSave}
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          {/* <p>
           <label>Title</label>
           <Input />
         </p>
@@ -49,10 +70,11 @@ export default function NewProject({ onAdd }) {
           <label>Due Date</label>
           <Input />
         </p> */}
-        <Input type="text" ref={title} label="Title" />
-        <Input ref={description} label="Description" isTextArea />
-        <Input type="date" ref={dueDate} label="Due Date" />
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" isTextArea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
